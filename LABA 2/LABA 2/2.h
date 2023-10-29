d3f88beca45eaf54b32c41b3270f1ed51113d1a0
polinazharova <127535664+polinazharova@users.noreply.github.com>
2023-10-29 12:58:04 +0300

    Something was created

---
+++ LABA 2/LABA 2/2.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//int by_iter(std::vector<int> v1, std::vector<char> v2) {
+//	std::cout << "Marks and names:" << std::endl;
+//	for (std::vector<int>::iterator i = v1.begin(); i != v1.end(); i++) {
+//		std::cout << *i << " ";
+//	}
+//	std::cout << "Names:" << std::endl;
+//	for (std::vector<int>::iterator i = v2.begin(); i != v2.end(); i++) {
+//		std::cout << *i << " ";
+//	}
+//	return 1;
+//}
---
+++ LABA 2/LABA 2/demo.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<int> values;
+std::vector<char> names;
+
+int demo(std::vector<int> v1, std::vector<char> v2) {
+	v1 = { 100, 40, 60, 70, 95, 95, 86, 77, 56, 51 };
+	v2 = { 'P', 'K', 'N', 'M', 'T', 'L', 'O', 'G', 'A' };
+}
