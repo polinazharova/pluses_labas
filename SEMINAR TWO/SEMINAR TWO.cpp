26f2a43edac888c64ef91a6a60856ba3cb6b0cbd
polinazharova <1132226165@pfur.ru>
2024-03-16 15:51:18 +0300

    Almost done

--- SEMINAR TWO/SEMINAR TWO.cpp
+++ SEMINAR TWO/SEMINAR TWO.cpp
@@ -22,82 +22,121 @@ public:
 
     ~Matrix() {
         std::cout << "Destruction." << std::endl;
-        for (int i{ 0 }; i < n; i++) 
-            delete [] data[i];
+        for (int i{ 0 }; i < n; i++) {
+            delete[] data[i];
+            data[i] = nullptr;
+        }
         delete[] data;
+        data = nullptr;
     }
 
-    void Filling() {
-        for (int i{ 0 }; i < n; i++) {
-            for (int j{ 0 }; j < m; j++) 
-                data[i][j] = rand() % 10;
-            
+    void filling() {
+        int choice;
+        std::cout << "Choose what to fill the matrix with: " << std::endl;
+        std::cout << "1 - RANDOM FILLING." << std::endl;
+        std::cout << "2 - UNIT FILLING." << std::endl;
+        std::cout << "3 - CUSTOM FILLING." << std::endl;
+        std::cin >> choice;
+        switch (choice) {
+        case 1:
+            for (int i{ 0 }; i < n; i++) {
+                for (int j{ 0 }; j < m; j++)
+                    data[i][j] = rand() % 10;
+            }
+            break;
+        case 2:
+            for (int i{ 0 }; i < n; i++) {
+                for (int j{ 0 }; j < m; j++) {
+                    if (j == i)
+                        data[i][j] = 1;
+                    else
+                        data[i][j] = 0;
+
+                }
+            }
+            break;
+        case 3: {
+            unsigned int element;
+            for (int i{ 0 }; i < n; i++) {
+                for (int j{ 0 }; j < m; j++) {
+                    std::cout << "Put in the element of " << i + 1 << " row " << j + 1 << " column: ";
+                    std::cin >> element;
+                    data[i][j] = element;
+                }
+            }
+            break;
+        }
+
+        default:
+            std::cout << "There was no such choice. The numbers will be randomized.";
+            for (int i{ 0 }; i < n; i++) {
+                for (int j{ 0 }; j < m; j++)
+                    data[i][j] = rand() % 10;
+            }
+            break;
         }
     }
 
-    void operator [] (int num) {
-        if (num > n * m || num < 0)
+    int& operator [] (unsigned int num) {
+        if (num >= n * m || num < 0)
             std::cout << "ERROR! INDEX IS NOT EXISTED!" << std::endl;
         else {
-            std::cout << data[0][0] << std::endl;
-            std::cout << "Would you like to chat that element?" << std::endl;
-
+            return data[num/m][num%m];
         }
     }
     
-    void operator << (Matrix& matrix) {
-        std::cout << "Your matrix is: " << std::endl;
+    void operator << (std::string gap) {
         for (int i{ 0 }; i < n; i++) {
             for (int j{ 0 }; j < m; j++)
-                std::cout << data[i][j] << " ";
+                std::cout << data[i][j] << gap;
             std::cout << std::endl;
         }
     }
 
-    void operator + (Matrix& other) {
+    Matrix& operator + (const Matrix& other) {
         if (n != other.n || m != other.m)
             std::cout << "CANNOT BE DONE. MATRIXES STRINGS OR COLLUMNS AREN'T EQUAL!" << std::endl;
         else {
-            std::cout << "Your summary matrix is: " << std::endl;
+            Matrix* demo = new Matrix(n,m);
             for (int i{ 0 }; i < n; i++) {
                 for (int j{ 0 }; j < m; j++)
-                    std::cout << data[i][j] + other.data[i][j] << " ";
-                std::cout << std::endl;
+                    (*demo).data[i][j] = data[i][j] + other.data[i][j];
             }
+            return *demo;
         }
     }
 
 
-    void operator - (Matrix& other) {
+    Matrix& operator - (const Matrix& other) {
         if (n != other.n || m != other.m)
             std::cout << "CANNOT BE DONE. MATRIXES STRINGS OR COLLUMNS AREN'T EQUAL!" << std::endl;
         else {
-            std::cout << "Your substracted matrix is: " << std::endl;
+            Matrix* demo = new Matrix(n,m);
             for (int i{ 0 }; i < n; i++) {
                 for (int j{ 0 }; j < m; j++)
-                    std::cout << data[i][j] - other.data[i][j] << " ";
-                std::cout << std::endl;
+                    (*demo).data[i][j] = data[i][j] - other.data[i][j];
             }
+            return *demo;
         }
     }
 
-    void operator * (Matrix& other) {
+    Matrix& operator * (Matrix& other) {
         if (m != other.n)
             std::cout << "CANNOT BE DONE!" << std::endl;
         else {
-            int temp{ 0 };
-            std::cout << "Your multiplied matrix is: " << std::endl;
-            for (int i{ 0 }; i < n; i++) {
+            Matrix* demo = new Matrix(n, other.m);
+            unsigned int temp{ 0 };
+            for (int i{ 0 }; i < m; i++) {
                 for (int j{ 0 }; j < m; j++) {
-                        temp += data[i][j] * other.data[j][i];
-                        if (j == m - 1) {
-                            std::cout << temp << " ";
-                            temp = 0;
-                            break;
-                        }
+                    for (int k{ 0 }; k < m; k++) {
+                        temp += data[j][k] * other.data[k][i];
+                    }
+                    (*demo).data[j][i] = temp;
+                    temp = 0;
                 }
-                std::cout << std::endl;
-            }
+           }
+            std::cout << demo << std::endl;
+            return *demo;
         }
     }
 
@@ -157,21 +196,21 @@ public:
 int main()
 {
     srand(time(NULL));
+    setlocale(LC_ALL, "rus");
 
     Matrix m;
-    Matrix s(3,5);
-    m.Filling();
-    s.Filling();
-    //m << m;
-    //s << s;
-    m * s;
-    //std::cout << (m == s) << std::endl;
-    /*m << m;
-    s << s;
-    m + s;
-    m << m;
-    s << s;*/
- 
+    Matrix s;
+    m.filling();
+    m[3] = 3;
+    m << " ";
+    /*m[11];*/
+   // s.filling();
+   // Matrix& ms = m * s;
+
+   // std::cout << &ms << std::endl;
+   ///* ms << "|";*/
+   // delete &ms;
+  
 }
 
 
